80a671e9048973ef369db14f068fcf147481746a
EO <ogunyaeniola@gmail.com>
2024-04-24 20:51:39 +0100

    completed

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,10 +1,15 @@
 const answer = []
 
 // Write your code below this line
-
-
-
-
-
+for (let number = 1; number <= 15; number++) {
+  if (number % 15 === 0) {
+    answer.push('FizzBuzz')
+ } else if (number % 3 === 0) {
+     answer.push('Fizz')
+ } else if (number % 5 === 0) {
+    answer.push('Buzz')
+ } else answer.push(number)
+}
+console.log(answer)
 // Don't touch the code below this line, we'll cover it later
 module.exports = answer
